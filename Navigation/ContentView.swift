5c90d063e8bd0761301943e381ef2a3c3aa66682
Jan Hovland <jan.hovland@lyse.net>
2019-09-16 19:11:01 +0200

    test

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -9,8 +9,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var firstName = ""
+    @State var lastName = ""
+
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            VStack {
+                VStack {
+                    VStack {
+                        Group {
+                            TextField("First name", text: $firstName)
+                            .padding(12)
+                        } .background(Color.white)
+                          .clipShape(RoundedRectangle(cornerRadius: 5))
+                          .shadow(radius: 5)
+                        
+                        Group {
+                            TextField("Last name", text: $lastName)
+                            .padding(12)
+                        } .background(Color.white)
+                          .clipShape(RoundedRectangle(cornerRadius: 5))
+                          .shadow(radius: 5)
+                                              
+                        Button(action: {
+                            
+                        }) {
+                            Group {
+                                Text("Create user")
+                                    .foregroundColor(.white)
+                                    .padding(12)
+                            } .background(Color.blue)
+                              .clipShape(RoundedRectangle(cornerRadius: 5))
+                            .shadow(radius: 5)
+                        }
+
+                        } .padding(12)
+                    
+                } .background(Color.gray)
+              
+                List {
+                    Text("EMPTY BOX")
+                }
+            } .navigationBarTitle("Credit Card Form")
+                .navigationBarItems(leading: HStack {
+                    Text("First Name:")
+                    Text(firstName)
+                   .foregroundColor(Color.red)
+                    
+                    Text("Last Name:")
+                    Text(lastName)
+                    .foregroundColor(Color.red)
+               })
+         }
     }
 }
 
